fd7d995fe77a06322c60edac7383a272abdb3206
slinfotec <slinfotecsoftware@gmail.com>
2024-10-07 01:28:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This site is temporarily unavailable</h1>
+        <p>Please check again later.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
